e07284c96bd30721b8b4825a9f5377c29c37a60d
Levi080 <leviribeiro0800@gmail.com>
2025-09-16 17:31:47 -0300

    main: criação da solution e criação dos projetos "AmaisEducacao.Data", "AmaisEducacao.Domain" e "AmaisEducacao.API". Criação da entidade "Student".

---
+++ api/AmaisEducacaoSolution/AmaisEducacao.Domain/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿
+namespace AmaisEducacao.Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public string CPF { get; set; }
+    }
+}
