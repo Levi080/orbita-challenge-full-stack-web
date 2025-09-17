8bebf5f8c8bc0bafb12029758a77ebaa843d0b5b
Levi080 <leviribeiro0800@gmail.com>
2025-09-17 16:12:28 -0300

    feat: criação da estrutura inicial do projeto frontend.

---
+++ web/amais-educacao-web/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+      <v-app-bar app color="primary" dark>
+        <v-toolbar-title>Gestão de Alunos</v-toolbar-title>
+      </v-app-bar>
+  
+      <v-main>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup>
+
+  </script>
+  
+  <style>
+
+  </style>
